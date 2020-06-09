61934ca4037b3999a7abfa1a56b1fde3abe16187
Randall Roberts <randall@randallroberts.ca>
2020-06-09 14:32:46 -0400

    Initial MERN setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/*
+Start the mongodb daemon by running C:\mongodb\bin\mongod.exe in the Command Prompt. Or by running, C:\Program Files (x86)\MongoDB\Server\4.2\bin\mongod.exe
+
+Connect to MongoDB using the Mongo shell While the MongoDB daemon is running, from a different Command prompt window run C:\Program Files\MongoDB\Server\4.2\bin\mongo.exe
+*/
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const db = require('./data/db')
+const app = express()
+const apiPort = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
